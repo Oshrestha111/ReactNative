a4ac93b9070288655362fe31a1327ab58a3771ed
Oriya <oriya.shrestha111@gmail.com>
2022-09-02 12:53:03 +0545

    changed color

--- todo/components/Header.js
+++ todo/components/Header.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   header: {
     height: 60,
     paddingTop: 18,
-    backgroundColor: "#C7CEEA",
+    backgroundColor: "#018749",
   },
   title: {
     textAlign: "center",
