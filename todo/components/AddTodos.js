96bcd3463cea677578b8efffe2920651c50a961f
Oriya <oriya.shrestha111@gmail.com>
2022-09-02 12:52:55 +0545

    added delete and plus icon

--- todo/components/AddTodos.js
+++ todo/components/AddTodos.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function AddTodo({ submitHandler }) {
   const [text, setText] = useState("");
@@ -9,23 +17,36 @@ export default function AddTodo({ submitHandler }) {
   };
 
   return (
-    <View>
+    <View style={styles.inputBar}>
       <TextInput
         placeholder="new todo...."
         style={styles.input}
         onChangeText={changeHandler}
       />
-      <Button
+
+      <TouchableOpacity onPress={() => submitHandler(text)}>
+        <Text style={{ paddingTop: 8 }}>
+          <AntDesign name="pluscircle" size={32} color="green" />
+        </Text>
+      </TouchableOpacity>
+
+      {/* <Button
         title="add todo"
-        color="#B5EAD7"
+        color="#018749"
         onPress={() => submitHandler(text)}
-      />
+      /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  inputBar: {
+    flexDirection: "row",
+    // justifyContent: "space-between",
+    alignContent: "flex-end",
+  },
   input: {
+    flex: 4,
     marginBottom: 10,
     padding: 8,
     // color: "#fff",
--- todo/components/TodoItem.js
+++ todo/components/TodoItem.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
 const TodoItem = ({ item, pressHandler }) => {
   return (
-    <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
-    </TouchableOpacity>
+    <View style={styles.item}>
+      <Text>{item.text}</Text>
+      <TouchableOpacity onPress={() => pressHandler(item.key)}>
+        <MaterialIcons name="delete" size={24} color="grey" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: 16,
     marginTop: 16,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderStyle: "dashed",
+    // borderColor: "#ddd",
+    // borderWidth: 1,
+    // borderStyle: "dashed",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderRightColor: "#ccc",
     borderRadius: 10,
+    borderLeftColor: "#ADFF2F",
+    borderLeftWidth: 6,
   },
 });
 
