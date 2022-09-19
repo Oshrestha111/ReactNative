91e8242e996d61c72793812fa3f1a37272901b5e
Oriya <oriya.shrestha111@gmail.com>
2022-09-19 11:11:51 +0545

    carousel not working

--- gaming-ui/App.js
+++ gaming-ui/App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/HomeScreen";
+import { useFonts } from "expo-font";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    robotoRegular: require("./src/assets/fonts/Roboto-Regular.ttf"),
+    robotoBold: require("./src/assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }} //header dekhaudaina
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
